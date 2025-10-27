3dcaf94b325e7fa018783693913857ff52d9c96a
AbdeIkader <abdelrahmanabdelkader2002@gmail.com>
2025-10-27 23:12:17 +0300

    Add logout button

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
   return (
     <div className="w-64 bg-gray-800 text-white h-screen fixed left-0 top-0 p-4">
       <h2 className="text-2xl font-bold mb-6 text-center">Dashboard</h2>
@@ -29,6 +34,14 @@ const Sidebar = () => {
           <Link to="/weather" className="hover:text-blue-400">
             Weather
           </Link>
+        </li>
+        <li>
+          <button
+            onClick={logout}
+            className="w-full bg-red-500 text-white p-2 rounded hover:bg-red-600 mt-6"
+          >
+            Logout
+          </button>
         </li>
       </ul>
     </div>
